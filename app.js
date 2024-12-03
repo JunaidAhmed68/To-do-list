1ed0652e4b5b6912bd9299f145e2c1c493cfdc77
JunaidAhmed68 <junaid042004@gmail.com>
2024-12-03 17:09:32 +0500

    not completed yet! working on it

---
+++ app.js
@@ -0,0 +1,339 @@
+// var usersData = [
+//   {
+//     name: "Junaid",
+//     rollNo: 123456,
+//     grade: 12,
+//     chemistry: 85,
+//     physics: 70,
+//     math: 95,
+//     english: 80,
+//     urdu: 73,
+//     pst: 45,
+//     status: "pass",
+//     password: "junaid1234",
+//     gmail: "junaid@gmail.com",
+//   },
+//   {
+//     name: "Ali",
+//     rollNo: 123457,
+//     grade: 12,
+//     chemistry: 90,
+//     physics: 75,
+//     math: 88,
+//     english: 85,
+//     urdu: 78,
+//     pst: 50,
+//     status: "pass",
+//     password: "ali20233",
+//     gmail: "ali@gmail.com",
+//   },
+//   {
+//     name: "Sara",
+//     rollNo: 123458,
+//     grade: 12,
+//     chemistry: 80,
+//     physics: 65,
+//     math: 92,
+//     english: 76,
+//     urdu: 70,
+//     pst: 42,
+//     status: "pass",
+//     password: "sara1234",
+//     gmail: "sara@example.com",
+//   },
+//   {
+//     name: "Kashan",
+//     rollNo: 123459,
+//     grade: 12,
+//     chemistry: 88,
+//     physics: 72,
+//     math: 90,
+//     english: 82,
+//     urdu: 75,
+//     pst: 48,
+//     status: "pass",
+//     password: "kashan2023",
+//     gmail: "kashan@gmail.com",
+//   },
+//   {
+//     name: "Nadia",
+//     rollNo: 123460,
+//     grade: 12,
+//     chemistry: 78,
+//     physics: 68,
+//     math: 85,
+//     english: 79,
+//     urdu: 72,
+//     pst: 44,
+//     status: "fail",
+//     password: "nadia1234",
+//     gmail: "nadia@example.com",
+//   },
+//   {
+//     name: "Zain",
+//     rollNo: 123461,
+//     grade: 12,
+//     chemistry: 92,
+//     physics: 80,
+//     math: 94,
+//     english: 90,
+//     urdu: 80,
+//     pst: 53,
+//     status: "pass",
+//     password: "zain2023",
+//     gmail: "zain@example.com",
+//   },
+//   {
+//     name: "Hina",
+//     rollNo: 123462,
+//     grade: 12,
+//     chemistry: 83,
+//     physics: 74,
+//     math: 89,
+//     english: 78,
+//     urdu: 71,
+//     pst: 47,
+//     status: "pass",
+//     password: "hina1234",
+//     gmail: "hina@example.com",
+//   },
+//   {
+//     name: "Sajid",
+//     rollNo: 123463,
+//     grade: 12,
+//     chemistry: 76,
+//     physics: 70,
+//     math: 88,
+//     english: 82,
+//     urdu: 74,
+//     pst: 49,
+//     status: "fail",
+//     password: "sajid2023",
+//     gmail: "sajid@example.com",
+//   },
+//   {
+//     name: "Ayesha",
+//     rollNo: 123464,
+//     grade: 12,
+//     chemistry: 89,
+//     physics: 78,
+//     math: 91,
+//     english: 87,
+//     urdu: 79,
+//     pst: 51,
+//     status: "pass",
+//     password: "ayesha1234",
+//     gmail: "ayesha@gmail.com",
+//   },
+//   {
+//     name: "Fahad",
+//     rollNo: 123465,
+//     grade: 12,
+//     chemistry: 82,
+//     physics: 69,
+//     math: 86,
+//     english: 81,
+//     urdu: 70,
+//     pst: 46,
+//     status: "pass",
+//     password: "fahad2023",
+//     gmail: "fahad@example.com",
+//   },
+//   {
+//     name: "Amna",
+//     rollNo: 123466,
+//     grade: 12,
+//     chemistry: 79,
+//     physics: 71,
+//     math: 93,
+//     english: 77,
+//     urdu: 68,
+//     pst: 43,
+//     status: "fail",
+//     password: "amna1234",
+//     gmail: "amna@example.com",
+//   },
+//   {
+//     name: "Usman",
+//     rollNo: 123467,
+//     grade: 12,
+//     chemistry: 84,
+//     physics: 73,
+//     math: 87,
+//     english: 80,
+//     urdu: 72,
+//     pst: 46,
+//     status: "pass",
+//     password: "usman2023",
+//     gmail: "usman@example.com",
+//   },
+//   {
+//     name: "Tariq",
+//     rollNo: 123468,
+//     grade: 12,
+//     chemistry: 90,
+//     physics: 76,
+//     math: 89,
+//     english: 83,
+//     urdu: 75,
+//     pst: 49,
+//     status: "fail",
+//     password: "tariq1234",
+//     gmail: "tariq@gmail.com",
+//   },
+// ];
+
+// localStorage.setItem('user',JSON.stringify(usersData))
+// var getData=JSON.parse(localStorage.getItem('user'))
+
+// //delete single user data
+// // var tempData=[]
+// // for (let i = 0; i < getData.length; i++) {
+// //    if(getData[i].name=='Ali'){
+// //     continue
+// //    }  
+// //    tempData=[...tempData,getData[i]]
+// // }
+// // getData=tempData
+// // localStorage.setItem('user',JSON.stringify(getData))
+
+// //rename/add/remove/update  user data
+
+// // var tempData=[]
+// // for (let i = 0; i < getData.length; i++) {
+// //    if(getData[i].name=='Ali'){
+// //      tempData=[...tempData,{...getData[i],}]
+// //      continue
+// //    }  
+// //    tempData=[...tempData,getData[i]]
+// // }
+// // console.log(getData)
+// // console.log(tempData)
+
+// for (let i = 0; i < 10; i++) {
+  //   mainDiv.innerHTML+=("<li class='list-group-item' id='li1'><div class='first'><input class='form-check-input me-1' type='checkbox' value='' id='firstCheckbox'><span class='form-check-label' for='firstCheckbox'>First checkbox  </span></div><div class='second'><span>2/2/2024</span><button   id='btn'  type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#staticBackdrop1'   >Edit</button><button  id='btn'  type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#staticBackdrop2'>Delete</button></div></li>")
+  // }
+  // console.log(document.getElementsByClassName("list-group")[0].children
+  // )
+
+
+// var data=[];
+// function addData(e){
+  //   var textAreaVal= document.getElementById("areaText").value
+  //   if(textAreaVal!==""){
+    //     console.log(textAreaVal)
+//     data=[...data,{text:textAreaVal}]
+//     localStorage.setItem('user_Data',JSON.stringify(data))
+//   }else{
+  //     console.log("chal")
+//   }
+// }
+// data= JSON.parse(localStorage.getItem('user_Data'))
+// localStorage.setItem('user_Data',JSON.stringify(data))
+
+// }
+// var getData = JSON.parse(localStorage.getItem('user_Data'))
+// var temp='kkk'
+// for (let i = 0; i < getData.length; i++) {
+  //   if (temp == getData[i].text) {
+    //     for (let i = 0; i <getData.length; i++) {
+      //         getData=[...getData,]      
+      //     }
+      //     break;
+      //   } else {
+        //     console.log(getData[i]);
+//   }
+// }data-bs-dismiss="modal"
+var orgForm = "<div class='modal-dialog'><div class='modal-content'><div class='modal-header'><h1 class='modal-title fs-5' id='staticBackdropLabel'>Add your new note</h1><button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button></div><div class='modal-body'>    <textarea id='areaText'  rows='4' cols='50' style='width: 100%;  border-color: #0093E9;' ></textarea>  </div>  <div class='modal-footer'><button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Close</button><button type='button'   class='btn btn-primary' onfocus='addData(event)'>Add</button></div></div></div>"
+var c_date = new Date()
+var mainDiv= document.getElementsByClassName("list-group")[0]
+c_date=c_date.getDate()+'-'+c_date.getMonth()+'-'+c_date.getFullYear()
+var data = [];
+// add text function
+function addData(e) {
+  var textAreaVal = document.getElementById("areaText").value;
+
+  if (textAreaVal !== "") {
+ 
+    mainDiv.innerHTML += "<li class='list-group-item' id='li1'>" +
+      "<div class='first'>" +
+        "<input class='form-check-input me-1' type='checkbox' value='' id='firstCheckbox' style='padding-right:5px'> " +
+        "<span class='form-check-label' for='firstCheckbox'>"+textAreaVal+"</span>" +
+      "</div>" +
+      "<div class='second'>" +
+        "<span>" + c_date + "</span>" + // Insert the formatted date here
+        "<button id='btn' type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#staticBackdrop1' onclick='editText(event)'>Edit</button>" +
+        "<button id='btn' type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#staticBackdrop2'>Delete</button>" +
+      "</div>" +
+    "</li>";
+    
+
+    var storedData = JSON.parse(localStorage.getItem('user_Data')) || [];
+    storedData = [...storedData, { text: textAreaVal }];
+    localStorage.setItem('user_Data', JSON.stringify(storedData));
+    
+    textAreaVal.value=''
+    document.getElementById("staticBackdrop").innerHTML="<div class='modal-dialog'><div class='modal-content'><div class='modal-header'><h1 class='modal-title fs-5' id='staticBackdropLabel'>Your list added sucessfully!</h1><button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close' onclick='ogForm()'></button></div></div></div>"
+  }
+}
+// getting orignal form afer message of sucession
+function ogForm(){
+  document.getElementById("staticBackdrop").innerHTML=orgForm;
+}
+// edit function
+var preText;
+var previousText;
+function editText(e){
+  previousText=e.target.parentElement.previousElementSibling.children[1].innerHTML;
+  document.getElementById("areaTexte").innerHTML=previousText
+  preText=previousText;
+}
+function editOnDatabase(){
+  var storedData = JSON.parse(localStorage.getItem('user_Data'))
+  var newText=document.getElementById("areaTexte").value
+  for (let i = 0; i < storedData.length; i++) {
+    if (storedData[i].text === preText) {
+      storedData[i].text = newText; 
+      localStorage.setItem('user_Data', JSON.stringify(storedData));
+      break
+  }
+}
+previousText=newText;
+mainDiv.innerHTML += "<li class='list-group-item' id='li1'>" +
+      "<div class='first'>" +
+        "<input class='form-check-input me-1' type='checkbox' value='' id='firstCheckbox' style='padding-right:5px'> " +
+        "<span class='form-check-label' for='firstCheckbox'>"+newText+"</span>" +
+      "</div>" +
+      "<div class='second'>" +
+        "<span>" + c_date + "</span>" + // Insert the formatted date here
+        "<button id='btn' type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#staticBackdrop1' onclick='editText(event)'>Edit</button>" +
+        "<button id='btn' type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#staticBackdrop2'>Delete</button>" +
+      "</div>" +
+    "</li>";
+}
+function dontChangeText(){
+  document.getElementById("areaTexte").value=preText
+}
+function deleteData(e) {
+  var storedData = JSON.parse(localStorage.getItem('user_Data'));
+
+  // Loop through the children of mainDiv in reverse order to avoid issues when removing elements
+  for (let i = mainDiv.children.length - 1; i >= 0; i--) {
+    for (let j = 0; j < storedData.length; j++) {
+      var textToCompare = mainDiv.children[i].children[0].children[1].innerHTML;
+      console.log("Comparing:", textToCompare, "with", storedData[j].text);
+
+     
+      if (textToCompare === storedData[j].text) {
+        mainDiv.children[i].remove();  
+        // chatgpt used lhis line
+        storedData = storedData.filter(item => item.text !== storedData[j].text);
+
+        return
+      }
+    }
+  }
+  localStorage.setItem('user_Data', JSON.stringify(storedData));
+}
+
+
